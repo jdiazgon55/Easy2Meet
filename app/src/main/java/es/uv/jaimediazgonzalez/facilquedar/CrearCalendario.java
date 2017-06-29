bb635c12ce452c3de1b7c8d1956cdeefd52024df
paella55 <jaimediaz3112@gmail.com>
2017-06-29 18:40:02 +0200

    Initial commit

---
+++ app/src/main/java/es/uv/jaimediazgonzalez/facilquedar/CrearCalendario.java
@@ -0,0 +1,17 @@
+package es.uv.jaimediazgonzalez.facilquedar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Familia Diaz on 27/06/2017.
+ */
+
+public class CrearCalendario  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_calendario);
+    }
+}
---
+++ app/src/main/java/es/uv/jaimediazgonzalez/facilquedar/PantallaInicial.java
@@ -0,0 +1,39 @@
+package es.uv.jaimediazgonzalez.facilquedar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PantallaInicial extends AppCompatActivity {
+
+    private Button crearCalendario, verCalendario, misCalendarios, instrucciones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicial);
+
+        crearCalendario = (Button) findViewById(R.id.crearCalendario);
+        verCalendario = (Button) findViewById(R.id.verCalendario);
+        misCalendarios = (Button) findViewById(R.id.misCalendarios);
+        instrucciones = (Button) findViewById(R.id.instrucciones);
+
+        /* LISTENERS */
+        crearCalendario.setOnClickListener(crearCalendarioListener);
+    }
+
+    /* LISTENERS */
+    final View.OnClickListener crearCalendarioListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            //Declaro el Intent
+            Intent explicit_intent;
+            //Instanciamos el Intent dandole:
+            explicit_intent = new Intent(PantallaInicial.this, CrearCalendario.class);
+            startActivity(explicit_intent);
+        }
+    };
+}
