b2955848881517adf3c813af6b7a53c575064ceb
paella55 <jaimediaz3112@gmail.com>
2017-07-14 01:00:46 +0200

    Añadido calendario con libreria independiente + botón de guardar y texto.

--- app/src/main/java/es/uv/jaimediazgonzalez/facilquedar/Calendario.java
+++ app/src/main/java/es/uv/jaimediazgonzalez/facilquedar/Calendario.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 /**
  * Created by Familia Diaz on 02/07/2017.
  */
 
+
 public class Calendario extends AppCompatActivity {
+
+    private MaterialCalendarView calendario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
+
+        calendario = (MaterialCalendarView) findViewById(R.id.calendarioView);
+        calendario.setSelectionMode(MaterialCalendarView.SELECTION_MODE_MULTIPLE);
     }
 }
--- app/src/main/java/es/uv/jaimediazgonzalez/facilquedar/CrearCalendario.java
+++ app/src/main/java/es/uv/jaimediazgonzalez/facilquedar/CrearCalendario.java
@@ -2,11 +2,9 @@ package es.uv.jaimediazgonzalez.facilquedar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 /**
  * Created by Familia Diaz on 27/06/2017.
